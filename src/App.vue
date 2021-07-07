a89dfc53e2f399b14aab55cb5e92a8dcc171d490
Kévin <kevin.joya@outlook.com>
2021-07-07 11:42:26 +0200

    Added compo read progress bar & logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <read-progress-bar
+    :percent="progressBarPercent"
+  >
+  </read-progress-bar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ReadProgressBar from './components/ReadProgressBar.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ReadProgressBar },
+
+  data() {
+    return {
+      percent: 0
+    }
+  },
+
+  created() {
+    this.handleScroll();
+  },
+
+  computed: {
+    progressBarPercent() {
+      const percentRes = this.percent;
+
+      return isNaN(percentRes) ? 0 : percentRes
+    }
+  },
+
+  methods: {
+    handleScroll() {
+      window.addEventListener('scroll', () => {
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (winScroll / height) * 100;
+
+        this.percent = Math.round(scrolled);
+      })
+    },
   }
 }
 </script>
@@ -22,5 +52,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 200vh;
 }
 </style>
